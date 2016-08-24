bbf2269a3c5d4f310f12a3579803a67a8181e1e9
Stephen-Njoroge <steven.n360@gmail.com>
2016-08-25 01:23:51 +0300

    A functionality to allow users to access song's lyrics using track_id
    This commit adds functionality to the code to ensure that a user can
    get the lyrics for a specific song using the song's musixmatch track_id
    more to that, it removed the necessity for importing authentification
    to avoid creation of cache files that were affecting perfomance.
    The user can copy and paste a track_id into a function and thus get the lyrics.
    
    ref #8

--- lyrics-finder.py
+++ lyrics-finder.py
@@ -1,8 +1,9 @@
 import urllib.request, urllib.error, urllib.parse
 import json
 import socket
-from Auth import apikey_musixmatch
+import musixmatch
 from tabulate import tabulate
+apikey_musixmatch = "0f608f8ccf875fcdd9b26cb6d0da8284"
 
 apiurl_musixmatch = 'http://api.musixmatch.com/ws/1.1/'
 
@@ -11,12 +12,9 @@ def find_my_song(query_name):
 	This function receives a users querry be it an artist, track_name or words in the
 	songs database and returns songs that matches with the querry accompanied by a 
 	unique ID and Artist Name.
-
 	'''
 
-	querystring = (("{0} track.search?q={1} &apikey={2} &format=json&f_has_lyrics=1") 
-
-		.format(apiurl_musixmatch, urllib.parse.quote(query_name), apikey_musixmatch))
+	querystring = (apiurl_musixmatch + "track.search?q=" + (urllib.parse.quote(query_name)) + "&apikey=" + apikey_musixmatch + "&format=plain" + "&f_has_lyrics=1")
 
 	request = urllib.request.Request(querystring)
 
@@ -27,13 +25,11 @@ def find_my_song(query_name):
 	It is thus we add a user agent of some sort.
 	'''
 
-	request.add_header("User-Agent", "curl/7.9.8 (i686-pc-linux-gnu)"\
-
-		"libcurl 7.9.8 (OpenSSL 0.9.6b) (ipv6 enabled)")
+	request.add_header("User-Agent", "curl/7.9.8 (i686-pc-linux-gnu)libcurl 7.9.8 (OpenSSL 0.9.6b) (ipv6 enabled)")
 
 	try:
 
-		response = urllib.request.urlopen(request, timeout=30) #timeout set to 30 seconds
+		response = urllib.request.urlopen(request, timeout=3600) #timeout set to 3600 seconds
 
 		raw = response.read()
 
@@ -42,16 +38,15 @@ def find_my_song(query_name):
 		print("Timeout raised and caught")
 
 	json_obj = json.loads(raw.decode('utf-8'))
+	#print (json_obj)
 
 	body = json_obj["message"]["body"]["track_list"]
-
+	#print (body)
 	'''
-
-	A reminder on how to access track_list objects >>>[0]["track"]["track_id"] #["track_name"]
+	A reminder on how to access track_list objects >>>[0]["track"]["track_id"] or ["track_name"]
 	>>>>>
 	print (len(body)) Testing Why The Code only printed 10 objects, I found out the trial version
 	only allowed for 10 objects per search.
-
 	'''
 
 	i = 0
@@ -64,13 +59,7 @@ def find_my_song(query_name):
 
 		songs = body[i]
 
-		track_id = (songs["track"]["track_id"]) 
-		
-		'''
-		This is the Unique Track Id for every particular song, It will
-		Be necessary when searching for a songs lyrics.
-
-		'''
+		track_id = (songs["track"]["track_id"])
 
 		track_name = (songs["track"]["track_name"])
 
@@ -98,3 +87,41 @@ if query_name == "":
 else:
 
 	print(find_my_song(query_name))
+
+def get_lyrics(track_id):
+	querystring = (apiurl_musixmatch + "track.lyrics.get?track_id=" + (urllib.parse.quote(track_id)) + "&apikey=" + apikey_musixmatch + "&format=json" + "&f_has_lyrics=1")
+
+	request = urllib.request.Request(querystring)
+
+	request.add_header("Authorization", "Bearer " + apikey_musixmatch)
+
+	'''
+	Must include user agent of some sort, otherwise 403 returned
+	It is thus we add a user agent of some sort.
+	'''
+
+	request.add_header("User-Agent", "curl/7.9.8 (i686-pc-linux-gnu)libcurl 7.9.8 (OpenSSL 0.9.6b) (ipv6 enabled)")
+
+	try:
+
+		response = urllib.request.urlopen(request, timeout=3600) #timeout set to 3600 seconds
+
+		raw = response.read()
+
+	except socket.timeout:
+
+		print("Timeout raised and caught")
+
+	json_obj = json.loads(raw.decode('utf-8'))
+	#print (json_obj)
+
+	body = json_obj["message"]["body"]["lyrics"]["lyrics_body"]
+	copyright = json_obj["message"]["body"]["lyrics"]["lyrics_copyright"]
+	lyrics = (body + "\n\n" +copyright)
+	return lyrics
+print("="*70)
+track_id = input("Paste your Song's Track ID to get lyrics: ")
+print("="*50)
+print ("Enjoy Your Tune")
+print("="*40)
+print (get_lyrics(track_id))
